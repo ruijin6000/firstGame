e3311c7aafc4f8de7f72ed4f6b9aa974ffc30af2
RuiJin <rui.jin@sjsu.edu>
2018-10-13 03:28:41 -0700

    more added

---
+++ HW1/Assets/Script/Player/Collision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        print(col.gameObject.name);
+        if (col.gameObject.name == "Monster_front") {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+       
+
+    }
+}
