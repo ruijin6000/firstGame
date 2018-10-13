e3af75e0ebc2d507aaa0717f49df9fe5f9545838
RuiJin <rui.jin@sjsu.edu>
2018-10-13 04:40:31 -0700

    lateset Version

---
+++ HW1/Assets/Script/Monster/Destroyer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroyer : MonoBehaviour{
+    public float lifeTime = 50f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        //if (lifeTime >0) {
+        //    lifeTime -= Time.deltaTime;
+        //    if (lifeTime <= 0) {
+        //        destruction();
+        //    }
+        //}
+
+    }
+
+
+    void destruction() {
+        Destroy(this.gameObject);
+    }
+
+}
--- HW1/Assets/Script/Player/Collision.cs
+++ HW1/Assets/Script/Player/Collision.cs
@@ -12,7 +12,7 @@ public class Collision : MonoBehaviour {
     private void OnCollisionEnter2D(Collision2D col)
     {
         print(col.gameObject.name);
-        if (col.gameObject.name == "Monster_front") {
+        if (col.gameObject.name == "Monster_front" || col.gameObject.name =="Monster_front(Clone)") {
             Destroy(gameObject);
         }
     }
