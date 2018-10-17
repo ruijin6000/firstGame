f9bfe73ee11ee9501fdefa757fa89e1da910c044
RuiJin <rui.jin@sjsu.edu>
2018-10-16 22:05:25 -0700

    control update

---
+++ HW1/Assets/Script/InPut_Manager/FaceDirection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceDirection : AbstractBehavior {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var right = inputState.GetButtonValue(inputButtons[0]);
+        var left = inputState.GetButtonValue(inputButtons[1]);
+        var up = inputState.GetButtonValue(inputButtons[2]);
+        var down = inputState.GetButtonValue(inputButtons[3]);
+    
+    }
+}
---
+++ HW1/Assets/Script/InPut_Manager/InPutManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public enum Buttons
+{
+    Right,
+    Left,
+    Up,
+    Down,
+    A,
+    B
+
+}
+
+public enum Condition {
+   GreaterThan,
+    LessThan
+}
+
+[System.Serializable]
+public class InputAxisState{
+    public string axisName;
+    public float offValue;
+    public Buttons button;
+    public Condition condtion;
+
+    public bool value {
+        get {
+               var val = Input.GetAxis(axisName);
+                switch (condtion) {
+                case Condition.GreaterThan:
+                    return val > offValue;
+                case Condition.LessThan:
+                    return val < offValue;
+            }
+            return false;
+        }
+    }
+}
+
+public class InPutManager : MonoBehaviour{
+
+    public InputAxisState[] inputs;
+    public InPutState inputState;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var input in inputs) {
+            //Debug.Log("offvalue is " + Input.GetAxis("Horizontal"));
+            inputState.SetButtonValue(input.button, input.value);
+        }
+    }
+}
